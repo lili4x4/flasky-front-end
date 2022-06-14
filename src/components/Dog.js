559f8984b27c7bb6b467f8459349ec85453b10c8
Liliana <liliana.m.parra@gmail.com>
2022-06-14 13:44:53 -0400

    Created Dog and Doglist components

---
+++ src/components/Dog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Dog.css";
+
+const Dog = () => {
+  return (
+    <ul>
+      <li>
+        <h2>Dog Name</h2>
+      </li>
+    </ul>
+  );
+};
+
+export default Dog;
---
+++ src/components/DogList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./DogList.css";
+import Dog from "./Dog";
+
+const DogList = () => {
+  return <Dog />;
+};
+
+export default DogList;
